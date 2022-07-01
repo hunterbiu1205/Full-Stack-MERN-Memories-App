512767fc86521e58eb5ebab7c0e1f1684c2bee52
Hunter <hunterbiu1205@gmail.com>
2022-07-02 09:27:35 +0930

    Updates

---
+++ precache-manifest.af7796eeed3d9fe36509b33bde695f85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff34351a06565b478b5266e9568b135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad0f371a1bd5519f56b2",
+    "url": "/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "2e3cb51961a990141199",
+    "url": "/static/js/2.40f35edf.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.40f35edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0f371a1bd5519f56b2",
+    "url": "/static/js/main.619c9db4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.619c9db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
