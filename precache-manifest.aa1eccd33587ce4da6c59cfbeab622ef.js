0d0a14cd36bb8c7046d3c1bca1d2bbf6a27b2d17
Hunter <hunterbiu1205@gmail.com>
2022-07-02 12:04:23 +0930

    Updates

---
+++ precache-manifest.aa1eccd33587ce4da6c59cfbeab622ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0e47829f0745766d5257f378cba78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cc023bcc1c81d5564a5",
+    "url": "/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "136387d9375482f686c8",
+    "url": "/static/js/2.b46dcd98.chunk.js"
+  },
+  {
+    "revision": "652fe30beda1d11f18abc9cfdaebb924",
+    "url": "/static/js/2.b46dcd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc023bcc1c81d5564a5",
+    "url": "/static/js/main.5b2a2fa3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5b2a2fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Full-Stack-MERN-Memories-App/precache-manifest.dd1c3544fb9eaae3a14467df4d3d8c97.js"
+  "/precache-manifest.aa1eccd33587ce4da6c59cfbeab622ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Full-Stack-MERN-Memories-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
