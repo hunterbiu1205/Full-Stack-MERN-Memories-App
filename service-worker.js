4e75826f26bc20761043ec6f47aa44510f5f11d2
Hunter <hunterbiu1205@gmail.com>
2022-07-02 22:14:07 +0930

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Full-Stack-MERN-Memories-App/precache-manifest.0f343f6dae2bbef3ca07b4ffb9da70df.js"
+  "/precache-manifest.aa3551d42c6106ad0fe6fc2f6357547f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Full-Stack-MERN-Memories-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
