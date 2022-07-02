6ea6a8cefca41325f6d943e9a3db4b75db79c23c
Hunter <hunterbiu1205@gmail.com>
2022-07-02 15:31:45 +0930

    :sds

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://memories-app-354615.ts.r.appspot.com' });
 
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
